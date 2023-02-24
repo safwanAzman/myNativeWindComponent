fbbbf99951a3500c40aa709498d810583756e3d1
safwan <safwanazman017@gmail.com>
2023-02-24 21:27:51 +0800

    homescreen

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -3,54 +3,20 @@ import { Text, View } from 'react-native';
 import Container from '../../components/container';
 import Input from '../../components/form/Input';
 import Select from '../../components/form/Select';
+import Checkbox from 'expo-checkbox';
 
 
 export default function HomeScreen({navigation}) {
+    const [isChecked, setChecked] = useState(false);
     return (
         <Container>
-            <View className="mb-4">
-                <Input
-                    placeholder="input icon left"
-                    leftIcon="search"
+            {/* <View className="my-4">
+                <Checkbox
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? '#4630EB' : undefined}
                 />
-            </View>
-            <View className="mb-4">
-                <Input
-                    placeholder="input icon right"
-                    rightIcon="menu"
-                />
-            </View>
-            <View className="mb-4">
-                <Input
-                    label="input with label"
-                    placeholder="input with label"
-                />
-            </View>
-            <View className="mb-4">
-                <Input
-                    label="input with label & error message"
-                    placeholder="input with label & error message"
-                    errorMessage="Error Message"
-                />
-            </View>
-            <View className="mb-4">
-                <Select 
-                    label=""
-                    placeholder="select" 
-                    errorMessage=""
-                    options={[
-                        { label: 'Option 1', value: 'option1' },
-                        { label: 'Option 2', value: 'option2' },
-                        { label: 'Option 3', value: 'option3' },
-                    ]} 
-                    onSelectItem={(item) => {
-                        console.log(item);
-                    }}
-                    onChangeValue={(value) => console.log(value)}
-                    zIndex={3000}
-                    zIndexInverse={1000}
-                />
-            </View>
+            </View> */}
         </Container>
     );
 }
