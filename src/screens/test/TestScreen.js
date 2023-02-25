f736acfa9464f3a0ac266d0c594ac2f89fb0a998
safwan <safwanazman017@gmail.com>
2023-02-25 21:17:27 +0800

    add test screen

---
+++ src/screens/test/TestScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+export default function TestScreen() {
+    return (
+        <View className="items-center justify-center flex-1">
+            <Text>
+                TestScreen
+            </Text>
+        </View>
+    )
+}
